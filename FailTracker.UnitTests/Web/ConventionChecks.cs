505b586868c53de770a3fbfaf556c7c9b245e4a7
Matt Honeycutt <mbhoneycutt@gmail.com>
2011-06-19 01:51:09 -0500

    Added test to enforce "one-model-in" convention. Added a convention in the base controller that will perform validation if the input is a form.

--- FailTracker.UnitTests/Web/ConventionChecks.cs
+++ FailTracker.UnitTests/Web/ConventionChecks.cs
@@ -30,7 +30,7 @@ namespace FailTracker.UnitTests.Web
 
 			if (controllersWithWrongBase.Any())
 			{
-				Assert.Fail("The following controllers have the wrong base: " +
+				Assert.Fail("The following controllers have the wrong base: \r\n" +
 				            string.Join(", ", controllersWithWrongBase.ToArray()));
 			}
 		}
@@ -43,12 +43,34 @@ namespace FailTracker.UnitTests.Web
 			var unprotectedControllers = from c in controllers
 			                             where c.BaseType != typeof (AuthorizedFailTrackerController) &&
 			                                   c != typeof (AuthenticationController) &&
+											   c != typeof (SignUpController) &&
 											   c != typeof (UtilityController)
 			                             select c.Name;
 
 			if (unprotectedControllers.Any())
 			{
-				Assert.Fail("The following controllers should inherit from AuthorizedFailTrackerController: " + string.Join(", ", unprotectedControllers.ToArray()));
+				Assert.Fail("The following controllers should inherit from AuthorizedFailTrackerController: \r\n" + 
+					string.Join(", ", unprotectedControllers.ToArray()));
+			}
+		}
+
+		[Test]
+		public void All_controller_actions_follow_one_model_in_convention()
+		{
+			var controllers = GetControllers();
+
+			var actions = from c in controllers
+			              from a in c.GetMethods().Where(m => m.IsPublic && m.ReturnType.CanBeCastTo<ActionResult>())
+			              select a;
+
+			var badActions = from a in actions
+			                 where a.GetParameters().Count() > 1
+			                 select a.DeclaringType.Name + "." + a.Name;
+
+			if (badActions.Any())
+			{
+				Assert.Fail("All actions must have zero or one inputs only.  Correct the following actions: \r\n" +
+				            string.Join("\r\n", badActions.ToArray()));
 			}
 		}
 	}
--- FailTracker.Web/Controllers/FailTrackerController.cs
+++ FailTracker.Web/Controllers/FailTrackerController.cs
@@ -1,9 +1,32 @@
 using System.Web.Mvc;
+using System.Linq;
 
 namespace FailTracker.Web.Controllers
 {
 	public abstract class FailTrackerController : Controller
 	{
-		
+		protected override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			if (ActionNeedsServerSideValidation(filterContext))
+			{
+				if (!filterContext.Controller.ViewData.ModelState.IsValid)
+				{
+					HandleModelValidationFailure(filterContext);
+				}
+			}
+
+			base.OnActionExecuting(filterContext);
+		}
+
+		private static void HandleModelValidationFailure(ActionExecutingContext filterContext)
+		{
+			var result = new ViewResult { ViewData = new ViewDataDictionary(filterContext.Controller.ViewData) { Model = filterContext.ActionParameters.Values.Single() }};
+			filterContext.Result = result;
+		}
+
+		private static bool ActionNeedsServerSideValidation(ActionExecutingContext filterContext)
+		{
+			return filterContext.ActionDescriptor.GetParameters().Any(p => p.ParameterType.Name.Contains("Form"));
+		}
 	}
 }
