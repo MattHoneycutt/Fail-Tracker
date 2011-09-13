22f10e769fac45a66576e04cf88d5a20c3b40d88
matthoneycutt <mbhoneycutt@gmail.com>
2011-09-12 22:03:39 -0500

    Fixed a bug that caused only your most recently created project to show up in the project list, also made User unique by EmailAddress.

---
+++ FailTracker.Core/Data/ProjectOverrides.cs
@@ -0,0 +1,14 @@
+﻿using FailTracker.Core.Domain;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace FailTracker.Core.Data
+{
+	public class ProjectOverrides : IAutoMappingOverride<Project>
+	{
+		public void Override(AutoMapping<Project> mapping)
+		{
+			mapping.HasManyToMany<User>(p => p.Members);
+		}
+	}
+}
---
+++ FailTracker.Core/Data/UserOverrides.cs
@@ -0,0 +1,14 @@
+﻿using FailTracker.Core.Domain;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace FailTracker.Core.Data
+{
+	public class UserOverrides : IAutoMappingOverride<User>
+	{
+		public void Override(AutoMapping<User> mapping)
+		{
+			mapping.Map(u => u.EmailAddress).Unique();
+		}
+	}
+}
