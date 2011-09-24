3d0530e4ec5fb8d21b654bb5cbcc6198c1b0f1d4
matthoneycutt <mbhoneycutt@gmail.com>
2011-09-23 20:47:43 -0500

    Cleaned up some styles, added HtmlTags reference, and added a helper to make an image link.

---
+++ FailTracker.Web/Helpers/ImageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using HtmlTags;
+using Microsoft.Web.Mvc;
+
+namespace FailTracker.Web.Helpers
+{
+	public static class ImageExtensions
+	{
+		public static HtmlTag ImageLink<TController>(this HtmlHelper helper, Expression<Action<TController>> action, string imageRelativeUrl) where TController : Controller
+		{
+			var imageUrl = UrlHelper.GenerateContentUrl(imageRelativeUrl, helper.ViewContext.HttpContext);
+
+			var targetUrl = helper.BuildUrlFromExpression(action);
+
+			var link = new HtmlTag("a").Attr("href", targetUrl);
+
+			var image = new HtmlTag("img", link).Attr("src", imageUrl);
+
+			return link;
+		}
+	}
+}
